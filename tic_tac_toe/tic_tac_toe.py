7514b8eb7f319960b6616ef8cef49a34839655c8
RubikNube <roland@kiesendahl.de>
2023-06-19 22:54:38 +0200

    added simple computer player

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,14 +1,29 @@
 # A tic tac toe game that can be played against the computer or another player
 
+import random
+
 # Global variables
 board = [" "] * 10
 game_state = True
 announce = ""
 
+play_against_computer = False
+player_mark = "Not set yet"
+
 
 def play_game():
     reset_board()
-    global announce
+    global announce, play_against_computer, player_mark
+
+    # ask player if he wants to play against the computer
+    play_against_computer = (
+        input("Do you want to play against the computer? y/n ") == "y"
+    )
+
+    # ask player if he wants to be X or O
+    player_mark = input("Do you want to be X or O? ").upper()
+    while player_mark not in ["X", "O"]:
+        player_mark = input("Please choose X or O: ").upper()
 
     # Set marks
     X = "X"
@@ -18,17 +33,30 @@ def play_game():
         print("\n" * 100)
         display_board()
 
-        # Player X turn
-        game_state, announce = player_choice(X)
-        print(announce)
-        if game_state == False:
-            break
+        if(play_against_computer):
+            # Player X turn
+            game_state, announce = player_choice(X)
+            print(announce)
+            if game_state == False:
+                break
 
-        # Player O turn
-        game_state, announce = player_choice(O)
-        print(announce)
-        if game_state == False:
-            break
+            # Player O turn
+            game_state, announce = computer_choice(O)
+            print(announce)
+            if game_state == False:
+                break
+        else:
+            # Player X turn
+            game_state, announce = player_choice(X)
+            print(announce)
+            if game_state == False:
+                break
+
+            # Player O turn
+            game_state, announce = player_choice(O)
+            print(announce)
+            if game_state == False:
+                break
 
     # Ask player for a rematch
     rematch = input("Would you like to play again? y/n ")
@@ -40,9 +68,10 @@ def play_game():
 
 # Note: Game will ignore the 0 index
 def reset_board():
-    global board, game_state
+    global board, game_state, play_against_computer
     board = [" "] * 10
     game_state = True
+    play_against_computer = False
 
 
 def display_board():
@@ -64,7 +93,7 @@ def player_choice(mark):
     # Get Player Input
     mark = str(mark)
     # Validate input
-    ask_player(mark)
+    ask_player_for_move(mark)
 
     # Check for player win
     if win_check(board, mark):
@@ -84,6 +113,70 @@ def player_choice(mark):
 
     return game_state, announce
 
+def computer_choice(mark):
+    global board, game_state, announce
+    # Set game blank game announcement
+    announce = ""
+    # Get Player Input
+    mark = str(mark)
+
+    # search for a winning move
+    search_for_best_move(mark)
+
+    # Check for player win
+    if win_check(board, mark):
+        print("\n" * 100)
+        display_board()
+        announce = mark + " wins! Congratulations"
+        game_state = False
+
+    # Show board
+    print("\n" * 100)
+    display_board()
+
+    # Check for a tie
+    if full_board_check(board):
+        announce = "Tie!"
+        game_state = False
+
+    return game_state, announce
+
+def search_for_best_move(mark):
+    """Search for the best move to make"""
+    global board
+    # search for a winning move
+    for i in range(1, 10):
+        if board[i] == " ":
+            board[i] = mark
+            if win_check(board, mark):
+                return
+            else:
+                board[i] = " "
+
+    # search for a blocking move
+    if mark == "X":
+        mark = "O"
+    else:
+        mark = "X"
+    for i in range(1, 10):
+        if board[i] == " ":
+            board[i] = mark
+            if win_check(board, mark):
+                board[i] = "O"
+                return
+            else:
+                board[i] = " "
+
+    # search for a random move
+
+    return make_random_move(random)
+
+def make_random_move(random):
+    while True:
+        i = random.randint(1, 9)
+        if board[i] == " ":
+            board[i] = "O"
+            return
 
 def win_check(board, player):
     """Check Horizontals, Verticals, and Diagonals for a win"""
@@ -116,7 +209,7 @@ def win_check_diagonal(board, player):
     )
 
 
-def ask_player(mark):
+def ask_player_for_move(mark):
     """Asks player where to place X or O mark, checks validity"""
     global board
     req = "Choose where to place your " + mark + ": "
