ec049509fa4b9f81219acae75606f17a474c3a9c
RubikNube <roland@kiesendahl.de>
2023-06-19 22:27:16 +0200

    refactorings of the game

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tic-tac-toe
-A tic-tac-toe game written in python
+A console tic-tac-toe game written in python. You can play against another player or the computer. It's optimized to be played with the num-pad.
+
--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,15 +1,47 @@
 # A tic tac toe game that can be played against the computer or another player
 
 # Global variables
-board = [' '] * 10
+board = [" "] * 10
 game_state = True
 announce = ""
+
+
+def play_game():
+    reset_board()
+    global announce
+
+    # Set marks
+    X = "X"
+    O = "O"
+    while True:
+        # Show board
+        print("\n" * 100)
+        display_board()
+
+        # Player X turn
+        game_state, announce = player_choice(X)
+        print(announce)
+        if game_state == False:
+            break
+
+        # Player O turn
+        game_state, announce = player_choice(O)
+        print(announce)
+        if game_state == False:
+            break
+
+    # Ask player for a rematch
+    rematch = input("Would you like to play again? y/n ")
+    if rematch == "y":
+        play_game()
+    else:
+        print("Thanks for playing!")
 
 
 # Note: Game will ignore the 0 index
 def reset_board():
     global board, game_state
-    board = [' '] * 10
+    board = [" "] * 10
     game_state = True
 
 
@@ -18,77 +50,11 @@ def display_board():
     # Clear current cell output
     print("\n" * 100)
     # Print board
-    print("   |   |")
     print(" " + board[7] + " | " + board[8] + " | " + board[9])
-    print("   |   |")
     print("-----------")
-    print("   |   |")
     print(" " + board[4] + " | " + board[5] + " | " + board[6])
-    print("   |   |")
     print("-----------")
-    print("   |   |")
     print(" " + board[1] + " | " + board[2] + " | " + board[3])
-    print("   |   |")
-
-
-def win_check(board, player):
-    """Check Horizontals, Verticals, and Diagonals for a win"""
-    return (
-        win_check_horizontal(board, player)
-        or win_check_vertical(board, player)
-        or win_check_diagonal(board, player)
-    )
-
-
-def win_check_horizontal(board, player):
-    return (
-        (board[7] == board[8] == board[9] == player)
-        or (board[4] == board[5] == board[6] == player)
-        or (board[1] == board[2] == board[3] == player)
-    )
-
-def win_check_vertical(board, player):
-    return (
-        (board[7] == board[4] == board[1] == player)
-        or (board[8] == board[5] == board[2] == player)
-        or (board[9] == board[6] == board[3] == player)
-    )
-
-def win_check_diagonal(board, player):
-    return (
-        (board[1] == board[5] == board[9] == player)
-        or (board[3] == board[5] == board[7] == player)
-    )
-
-def full_board_check(board):
-    """Function to check if any remaining blanks are in the board"""
-    if " " in board[1:]:
-        return False
-    else:
-        return True
-
-
-def ask_player(mark):
-    """Asks player where to place X or O mark, checks validity"""
-    global board
-    req = "Choose where to place your " + mark + ": "
-    while True:
-        try:
-            choice = int(input(req))
-        except ValueError:
-            print("Sorry, please input a number between 1-9.")
-            continue
-
-        if choice not in range(1, 10):
-            print("Sorry, please input a number between 1-9.")
-            continue
-
-        if board[choice] == " ":
-            board[choice] = mark
-            break
-        else:
-            print("That space isn't empty!")
-
 
 
 def player_choice(mark):
@@ -119,36 +85,62 @@ def player_choice(mark):
     return game_state, announce
 
 
-def play_game():
-    reset_board()
-    global announce
+def win_check(board, player):
+    """Check Horizontals, Verticals, and Diagonals for a win"""
+    return (
+        win_check_horizontal(board, player)
+        or win_check_vertical(board, player)
+        or win_check_diagonal(board, player)
+    )
 
-    # Set marks
-    X = 'X'
-    O = 'O'
+
+def win_check_horizontal(board, player):
+    return (
+        (board[7] == board[8] == board[9] == player)
+        or (board[4] == board[5] == board[6] == player)
+        or (board[1] == board[2] == board[3] == player)
+    )
+
+
+def win_check_vertical(board, player):
+    return (
+        (board[7] == board[4] == board[1] == player)
+        or (board[8] == board[5] == board[2] == player)
+        or (board[9] == board[6] == board[3] == player)
+    )
+
+
+def win_check_diagonal(board, player):
+    return (board[1] == board[5] == board[9] == player) or (
+        board[3] == board[5] == board[7] == player
+    )
+
+
+def ask_player(mark):
+    """Asks player where to place X or O mark, checks validity"""
+    global board
+    req = "Choose where to place your " + mark + ": "
     while True:
-        # Show board
-        print("\n" * 100)
-        display_board()
+        try:
+            choice = int(input(req))
+        except ValueError:
+            print("Sorry, please input a number between 1-9.")
+            continue
 
-        # Player X turn
-        game_state, announce = player_choice(X)
-        print(announce)
-        if game_state == False:
+        if choice not in range(1, 10):
+            print("Sorry, please input a number between 1-9.")
+            continue
+
+        if board[choice] == " ":
+            board[choice] = mark
             break
+        else:
+            print("That space isn't empty!")
 
-        # Player O turn
-        game_state, announce = player_choice(O)
-        print(announce)
-        if game_state == False:
-            break
 
-    # Ask player for a rematch
-    rematch = input("Would you like to play again? y/n ")
-    if rematch == "y":
-        play_game()
-    else:
-        print("Thanks for playing!")
+def full_board_check(board):
+    """Function to check if any remaining blanks are in the board"""
+    return " " not in board[1:]
 
 
 play_game()
