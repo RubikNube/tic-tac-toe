d50508899cf147e614839a38ce9e3704e1380507
RubikNube <roland@kiesendahl.de>
2023-06-26 21:37:09 +0200

    added checks to the bash scripts

--- tic_tac_toe/executeStyleGuideChecks.sh
+++ tic_tac_toe/executeStyleGuideChecks.sh
@@ -1,4 +1,17 @@
+# !/bin/bash
 # Execute style guide checks
+
+# abort on nonzero exitstatus
+set -o errexit
+# abort on unbound variable
+set -o nounset
+# don't hide errors within pipes
+set -o pipefail
+
+script_name=$(basename "${0}")
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+readonly script_name script_dir
+
 # stop the build if there are Python syntax errors or undefined names
 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
--- tic_tac_toe/executeTests.sh
+++ tic_tac_toe/executeTests.sh
@@ -1,6 +1,19 @@
+# !/bin/bash
+# Executes the tests for the project.
+
+# abort on nonzero exitstatus
+set -o errexit
+# abort on unbound variable
+set -o nounset
+# don't hide errors within pipes
+set -o pipefail
+
+script_name=$(basename "${0}")
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+readonly script_name script_dir
+
 echo "Executing tests..."
 
-ls
 pushd ./tests
 python -m unittest discover
 popd
